2e13f55bd90d54ce5c5adbfda72b6d435d0f9c97
Ivan Demchuk <ivan.demchuk@gmail.com>
2022-06-17 15:26:30 +0300

    Switch to custom badge implementation

---
+++ src/components/Badge.vue
@@ -0,0 +1,44 @@
+<script setup>
+	defineProps({
+		title: { type: String, required: true },
+		icon: { type: String, required: true },
+		subtitle: { type: String, default: null },
+	})
+</script>
+
+<template>
+	<div class="badge font-mono">
+		<div class="title">
+			<div class="icon" :class="icon" />
+			{{ title }}
+		</div>
+		<div v-if="subtitle" class="subtitle">
+			{{ subtitle }}
+		</div>
+	</div>
+</template>
+
+<style>
+	.badge {
+		color: white;
+		display: flex;
+		flex-direction: row;
+	}
+	
+	.title {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		background-color: #273849;
+		padding: 0 0.5em;
+	}
+	
+	.icon {
+		margin-right: 0.25em;
+	}
+	
+	.subtitle {
+		background-color: #2bbc8a;
+		padding: 0 0.5em;
+	}
+</style>

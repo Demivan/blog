8455f49eca78103e9fe00571d71b9689dfcbc40e
Ivan Demchuk <ivan.demchuk@gmail.com>
2022-11-07 12:58:15 +0200

    Update NavBar.vue

--- layouts/components/NavBar.vue
+++ layouts/components/NavBar.vue
@@ -23,13 +23,13 @@ import ToggleTheme from './ToggleTheme.vue'
           <span class="lt-md:hidden">Projects</span>
           <div class="i-ri-lightbulb-line md:hidden" />
         </NuxtLink>
-        <NuxtLink to="https://mastodon.social/@Demivan" rel="me" title="Mastodon">
+        <NuxtLink to="https://mastodon.social/@Demivan" rel="me" target="_blank" title="Mastodon">
           <div class="i-ri-mastodon-line" aria-hidden="true" />
         </NuxtLink>
-        <NuxtLink to="https://twitter.com/IvanDemchuk" rel="me" title="Twitter">
+        <NuxtLink to="https://twitter.com/IvanDemchuk" rel="me" target="_blank" title="Twitter">
           <div class="i-feather-twitter" aria-hidden="true" />
         </NuxtLink>
-        <NuxtLink to="https://github.com/demivan" rel="me" title="GitHub">
+        <NuxtLink to="https://github.com/demivan" rel="me" "target"="_blank" title="GitHub">
           <div class="i-uil-github-alt" aria-hidden="true" />
         </NuxtLink>
         <ToggleTheme />

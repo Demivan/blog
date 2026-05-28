d79f846881e1041884656aa16ff9283345d22ce5
Ivan Demchuk <ivan.demchuk@gmail.com>
2026-05-28 15:50:16 +0300

    Restore emojis on frontpage

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@ definePageMeta({
 
 <template>
   <Page>
-    <p>Hi, I'm Ivan Demchuk. I'm Tech Lead, Full-Stack Developer working in Lviv, Ukraine.</p>
+    <p>Hi 👋, I'm Ivan Demchuk. I'm Tech Lead, Full-Stack Developer working in Lviv, Ukraine.</p>
 
     <p>
       Creator of <a href="https://github.com/demivan/fluent-vue">fluent-vue</a><br>
@@ -19,23 +19,23 @@ definePageMeta({
 
     <ul>
       <li>
-        I'm currently working on Vue.js i18n plugin - <code>fluent-vue</code><br>
+        🔭 I'm currently working on Vue.js i18n plugin - <code>fluent-vue</code><br>
         GitHub: <a href="https://github.com/fluent-vue/fluent-vue">fluent-vue</a><br>
         Docs: <a href="https://fluent-vue.demivan.me">fluent-vue.demivan.me</a><br>
       </li>
       <li>
-        I'm currently learning Nuxt 3
+        🌱 I'm currently learning Nuxt 3
       </li>
       <li>
-        Ask me about Vue, C#, Linux and Cosmere <UIcon name="custom-cosmere" class="dark-invert inline-block align-middle text-2xl" />
+        💬 Ask me about Vue, C#, Linux and Cosmere <UIcon name="custom-cosmere" class="dark-invert inline-block align-middle text-2xl" />
       </li>
       <li>
-        How to reach me: <a href="mailto:ivan.demchuk@gmail.com">ivan.demchuk@gmail.com</a>
+        📫 How to reach me: <a href="mailto:ivan.demchuk@gmail.com">ivan.demchuk@gmail.com</a>
       </li>
     </ul>
 
     <h3>
-      Technologies & Tools
+      🔧 Technologies & Tools
     </h3>
 
     <div class="flex flex-wrap gap-1 mb-4">
